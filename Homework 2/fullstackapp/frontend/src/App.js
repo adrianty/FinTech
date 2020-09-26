929882a286f115d0a8f3b319d8b094c09c1b72dc
Adrian Boo <70908950+adrianty@users.noreply.github.com>
2020-09-26 16:56:00 +0800

    Create App.js

---
+++ Homework 2/fullstackapp/frontend/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'; 
+import logo from './logo.svg'; 
+import './App.css'; 
+
+class App extends React.Component 
+{ 
+  constructor(props) 
+  { 
+    super(props); 
+    this.state = { serverResponse: "" }; 
+  } 
+  
+  callAPIServer() 
+  { 
+    fetch("http://localhost:7000/mypage")
+    .then(res => res.text())
+    .then(res => this.setState({ serverResponse: res }))
+    .catch(err => err); 
+  } 
+  
+  componentDidMount() 
+  { 
+    // react lifecycle method componentDidMount() //will execute the callAPIserver() method after the component mounts. 
+    this.callAPIServer(); 
+  } 
+  
+  render() 
+  { 
+    return ( 
+    <div className="App"> 
+    <header className="App-header"> 
+    <img src={logo} className="App-logo" alt="logo" /> 
+    <h1 className="App-title">Welcome to FintechSG React Course</h1> 
+    <h2 className="App-intro">{this.state.serverResponse}</h2> 
+    </header>
+    </div> );
+  }
+}
+export default App;
